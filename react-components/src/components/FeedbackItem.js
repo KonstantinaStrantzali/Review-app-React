faabab88e8d34d878ad2bf000ed8b3b0975c4883
KonstantinaStrantzali <konstrantz@gmail.com>
2022-09-13 09:38:58 +0000

    added close button functionality

--- react-components/src/components/FeedbackItem.js
+++ react-components/src/components/FeedbackItem.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import {FaTimes} from 'react-icons/fa'
+import Card from './shared/Card'
+import PropTypes from 'prop-types'
 
-export default function FeedbackItem({item}) {
+export default function FeedbackItem({item, handleDelete }) {
+ 
   
   return (
-    <div className='card'>
+    <Card>
     <div className='num-display'>{item.rating}</div>
+    <button onClick = {() => handleDelete(item.id)} className='close'>
+      <FaTimes color = "purple" />
+    </button>
     <div className='text-display'>{item.text}</div>
-    </div>
+    </Card>
   )
+}
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
 }
