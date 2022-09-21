d67d0ad97b5d24ea906746e357d63a1b5b287478
KonstantinaStrantzali <konstrantz@gmail.com>
2022-09-21 08:36:43 +0000

    calculate rating average functionality

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -4,20 +4,29 @@ import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './components/data/FeedbackData'
 import FeedbackItem from './components/FeedbackItem'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
 
 function App() {
+  
   const [feedback, setFeedback] = useState(FeedbackData)
+  
   const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')){
     setFeedback(feedback.filter((item) => item.id !== id))
   }
+}
   return (
     <>
     <div>
       <Header></Header>
+      <div className='container'>
+      <FeedbackForm></FeedbackForm>
+      <FeedbackStats feedback={feedback}></FeedbackStats>
       <FeedbackList feedback = {feedback} handleDelete = {deleteFeedback}></FeedbackList>
         
       
-
+    </div>
     </div>
     </>
   )
